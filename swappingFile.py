560fca3d7826754d8afedaba801847ce2538292b
Sayantan Dey <swatidey.samirdey@gmail.com>
2022-03-10 18:19:49 +0530

    Add files via upload

---
+++ swappingFile.py
@@ -0,0 +1,11 @@
+with open(file1, 'r') as a:
+    data_a = a.read()
+
+with open(file1, 'w') as a:
+    a.write(data_b)
+
+
+
+    File_object.read([sample1.txt])
+
+    File_object.write(sample2.txt)
